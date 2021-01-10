bdcadafb77a00966ba978bbce8636bb03aaecdb5
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-01-10 01:17:03 +0100

    reorderd some stuff

--- gauge.cpp
+++ gauge.cpp
@@ -30,6 +30,16 @@ SU2_mat SU2_mat::operator*(SU2_mat const &obj)
     return SU2_mat(res_c0, res_c1, res_c2, res_c3);
 }
 
+SU2_mat SU2_mat::dag()
+{
+    return SU2_mat(c0, -c1, -c2, -c3);
+}
+
+double SU2_mat::trace()
+{
+    return 2 * c0;
+}
+
 SU2_mat operator*(SU2_mat const &obj, double const &i)
 {
     return SU2_mat(obj.c0 * i, obj.c1 * i, obj.c2 * i, obj.c3 * i);
@@ -43,14 +53,4 @@ SU2_mat operator*(double const &i, SU2_mat const &obj)
 SU2_mat operator/(SU2_mat const &obj, double const &i)
 {
     return SU2_mat(obj.c0 / i, obj.c1 / i, obj.c2 / i, obj.c3 / i);
-}
-
-SU2_mat SU2_mat::dag()
-{
-    return SU2_mat(c0, -c1, -c2, -c3);
-}
-
-double SU2_mat::trace()
-{
-    return 2 * c0;
 }
